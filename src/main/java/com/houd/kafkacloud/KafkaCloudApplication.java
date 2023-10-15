ff889674aa59dd34e33d55b77c5c36c9818a914f
Fatima Ezzahra Houd <f.houd@etu.enset-media.ac.ma>
2023-10-15 12:59:36 +0100

    init project

---
+++ src/main/java/com/houd/kafkacloud/KafkaCloudApplication.java
@@ -0,0 +1,13 @@
+package com.houd.kafkacloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaCloudApplication.class, args);
+	}
+
+}
